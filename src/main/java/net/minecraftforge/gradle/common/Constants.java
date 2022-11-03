da46b1089410c659f6aba0dae5ce8e44ff3bd72c
maggi373 <40539743+maggi373@users.noreply.github.com>
2022-11-03 21:55:30 +0100

    update hosting for 1.7.10 jar files

--- src/main/java/net/minecraftforge/gradle/common/Constants.java
+++ src/main/java/net/minecraftforge/gradle/common/Constants.java
@@ -47,9 +47,9 @@ public class Constants {
     };
 
     // urls
-    public static final String MC_JSON_URL = "https://files.thorfusion.com/versions/{MC_VERSION}/{MC_VERSION}.json";
-    public static final String MC_JAR_URL = "https://files.thorfusion.com/versions/{MC_VERSION}/{MC_VERSION}.jar";
-    public static final String MC_SERVER_URL = "https://files.thorfusion.com/versions/{MC_VERSION}/minecraft_server.{MC_VERSION}.jar";
+    public static final String MC_JSON_URL = "https://gradle.thorfusion.com/versions/{MC_VERSION}/{MC_VERSION}.json";
+    public static final String MC_JAR_URL = "https://gradle.thorfusion.com/versions/{MC_VERSION}/{MC_VERSION}.jar";
+    public static final String MC_SERVER_URL = "https://gradle.thorfusion.com/versions/{MC_VERSION}/minecraft_server.{MC_VERSION}.jar";
     public static final String MCP_URL = "https://files.minecraftforge.net/fernflower-fix-1.0.zip";
     public static final String ASSETS_URL = "https://resources.download.minecraft.net";
     public static final String LIBRARY_URL = "https://libraries.minecraft.net/";
